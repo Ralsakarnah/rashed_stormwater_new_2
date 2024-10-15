5f312c0b01317f1a583378c201bb2484d6a1a9cc
MuhammadArbiIIT <marbi@hawk.iit.edu>
2024-10-14 22:42:48 -0500

    update the column names for length

--- static/js/data.js
+++ static/js/data.js
@@ -1,9 +1,9 @@
-function getColorClass(depth) {
-    if (depth < 50) {
+function getColorClass(length) {
+    if (length < 50) {
         return 'depth-green';
-    } else if (depth >= 50 && depth < 100) {
+    } else if (length >= 50 && length < 100) {
         return 'depth-yellow';
-    } else if (depth >= 100 && depth < 150) {
+    } else if (length >= 100 && length < 150) {
         return 'depth-orange';
     } else {
         return 'depth-red';
@@ -14,7 +14,8 @@ function fetchDataAndUpdate() {
     fetch('/api/data')
         .then(response => response.json())
         .then(data => {
-            console.log('Fetched data:', data); // Add this line to log the data
+            console.log('Fetched data:', data); // This line logs fetched data
+
             const tableBody = document.getElementById('data-table');
             tableBody.innerHTML = ''; // Clear the old table rows
 
@@ -23,13 +24,14 @@ function fetchDataAndUpdate() {
                 row.innerHTML = `
                     <td>${item.date.split(' ')[0]}</td>
                     <td>${item.time}</td>
-                    <td class="${getColorClass(item.depth)}">${item.depth ? item.depth.toFixed(2) : 'N/A'}</td>
+                    <td class="${getColorClass(item.length)}">${item.length ? item.length.toFixed(2) : 'N/A'}</td>
                 `;
                 tableBody.appendChild(row);
             });
         })
         .catch(error => console.error('Error fetching data:', error));
 }
+
 
 // Polling every 10 seconds
 setInterval(fetchDataAndUpdate, 10000); // 10,000 ms = 10 seconds
