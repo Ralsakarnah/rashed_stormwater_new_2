fed7356219ea7617e3950adab4224f71ebbcc3ba
MuhammadArbiIIT <marbi@hawk.iit.edu>
2024-10-14 21:57:36 -0500

    Updated Code for Render gs.py

--- gs.py
+++ gs.py
@@ -1,6 +1,6 @@
 import os
-import json
 import gspread
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 
@@ -19,10 +19,14 @@ else:
     raise ValueError("Google credentials not found in environment")
 
 def get_google_sheet_data():
-    # Open the Google Sheet by URL
-    sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/e/2PACX-1vRhcARj557XHf_PaANggXNm55G0AdTQ16lkQvIiJhiOg1LHkK0jBTtW48Yae1Ds81Yjqc6ne0bd2haA/pubhtml")
-    worksheet = sheet.get_worksheet(0)  # Open the first worksheet
+    try:
+        # Use the correct Google Sheets URL
+        sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1DAF8vwF2-2SedpmdOa-qycuYarnArRGFzWaDx-4dicc/edit?gid=634803012#gid=634803012")
+        worksheet = sheet.get_worksheet(0)  # Open the first worksheet
 
-    # Get all records from the sheet
-    data = worksheet.get_all_records()
-    return pd.DataFrame(data)
+        # Get all records from the sheet
+        data = worksheet.get_all_records()
+        return pd.DataFrame(data)
+    except Exception as e:
+        print(f"Error fetching data from Google Sheets: {str(e)}")
+        return pd.DataFrame()
